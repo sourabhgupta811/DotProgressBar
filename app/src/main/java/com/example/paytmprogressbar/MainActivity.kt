a0f2ade692f5f5a0fef95b3c62f13e33e9b06347
sourabhgupta811 <sourabh@totalitycorp.com>
2019-08-10 14:28:42 +0530

    initial commit

--- app/src/main/java/com/example/paytmprogressbar/MainActivity.kt
+++ app/src/main/java/com/example/paytmprogressbar/MainActivity.kt
@@ -2,7 +2,7 @@ package com.example.paytmprogressbar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.view.View
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -10,14 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        start.setOnClickListener {
-            if (dotProgressBar.isAnimationRunning())
-                dotProgressBar.stopAnimation()
-            else
-                dotProgressBar.startAnimation()
-        }
-        Handler().postDelayed({
-            dotProgressBar.setDotBackground(R.drawable.android)
-        },3000)
+        start.visibility = View.GONE
     }
 }
